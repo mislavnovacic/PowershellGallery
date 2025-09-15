8dbf9b867199eae78985a315970a43396e932029
Mislav Novacic <55797764+mislavnovacic@users.noreply.github.com>
2025-09-15 12:50:27 +0000

    Add files via upload

---
+++ DisablePW2.0.ps1
@@ -0,0 +1,11 @@
+﻿$featureVariables = [ordered]@{
+    Online      = $true
+    FeatureName = "MicrosoftWindowsPowerShellV2Root"
+}
+
+#Logging
+Write-Host "Disable: $($featureVariables.FeatureName)"
+$featureVariables | Format-Table -AutoSize | Write-output
+
+#Disable Windows Feature
+Disable-WindowsOptionalFeature @featureVariables
